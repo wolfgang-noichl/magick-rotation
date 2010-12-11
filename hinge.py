5e5b47a0c49cec6a49aa55eaa60dae618d183909
Favux <favux.is@gmail.com>
2010-12-11 13:04:32 -0600

    Add Dell XT & XT2 tablet pc's:  changes to 62-magick.rules, hinge.py, and hpwmi.py replaced with oem_wmi.py.

--- hinge.py
+++ hinge.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
+
 from commands import getstatusoutput
-from hpwmi import *
+from oem_wmi import *
 
 class hinge:
     def __init__(self, win):
-        self.hingelist = {"hpwmi":"/dev/input/hp-wmi"}
+        self.hingelist = {"oem_wmi":["/dev/input/hp-wmi", "/dev/input/dell-wmi"]}
         self.win = win
 
     def get_switch(self):
         ret_switch = None
-        for switch, filename in self.hingelist.iteritems():
-            command = "stat " + filename
-            if not getstatusoutput(command)[0]:
-                ret_switch = hpwmi(self.win)
-                break
+        for switch, filename_list in self.hingelist.iteritems():
+            for filename in filename_list:
+                command = "stat " + filename
+                if not getstatusoutput(command)[0]:
+                    ret_switch = oem_wmi(self.win)
+                    break
         return ret_switch
 
 if __name__ == "__main__":
     h = hinge()
-    switch = h.get_swtich()
+    switch = h.get_switch()
         
 #vim :set expandtab :set tabstop=4 :set shiftwidth=4
